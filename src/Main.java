5409dd41fc8592598cb209c9b8c4463203a93d49
ZionBlade <wernerct@mail.uc.edu>
2022-10-27 13:04:13 -0400

    third Commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        int[] dataPoints = new int[100];
+        Random gen = new Random();
+
+        for(int r = 0; r < dataPoints.length; r++)
+        {
+            dataPoints[r] = gen.nextInt(100) + 1;
+        }
+
+        for(int r = 0; r < dataPoints.length; r++)
+        {
+            System.out.print(dataPoints[r] + " | ");
+        }
     }
 }
